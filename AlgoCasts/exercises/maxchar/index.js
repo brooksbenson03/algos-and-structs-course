708d76b3688c1260bed6701fd326c1a5848994de
Brooks Benson <brooksbenson03@gmail.com>
2021-01-02 17:04:06 -0700

    maxchar

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {}
+
+  for (let c of str) {
+    chars[c] = chars[c] + 1 || 1
+  }
+ 
+  const max = Object.keys(chars).reduce((max, c) => {
+    return chars[max] > chars[c] ? max : c
+  })
+
+  return max
+}
 
 module.exports = maxChar;
